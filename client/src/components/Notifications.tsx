ace1fcf621b1c504c6fa096923146c955baf038b
Emmyojile <emmaojile99@gmail.com>
2023-10-23 15:03:33 -0800

    pushing to github

--- client/src/components/Notifications.tsx
+++ client/src/components/Notifications.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Notifications = () => {
   return (
-    <div className='h-12 bg-blue-700 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>Free Delivery for all orders over $50. order your food now!</div>
-  )
-}
+    <div className="h-12 bg-blue-700 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer">
+      Free Delivery for all orders over $50. order your food now!
+    </div>
+  );
+};
 
-export default Notifications
+export default Notifications;
