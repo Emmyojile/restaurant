991500dbec8e731f6d6d61f993ec2bdf14c1a1ce
Emmyojile <emmaojile99@gmail.com>
2023-10-24 11:49:40 -0800

    components

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import Featured from "@/components/Featured";
+import Offer from "@/components/Offer";
+import Slider from "@/components/Slider";
 
 export default function Home() {
-  return <main className="">Hello world</main>;
+  return <main className="">
+    <Slider/>
+    <Featured/>
+    <Offer/>
+  </main>;
 }
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -7,7 +7,7 @@ import CartIcon from "./CartIcon";
 const Navbar = () => {
   const user = false;
   return (
-    <div className="h-12 md:h-24 text-grey p-4 flex items-center justify-between border-b-2 border-b-blue-700 uppercase lg:px-20 xl:px-40">
+    <div className="h-12 md:h-24 text-grey p-4 flex items-center justify-between border-b-2 border-b-blue-700 uppercase lg:px-20 xl:px-40 bg-fuchsia-50">
       {/* LEFT LINKS */}
 
       <div className="hidden md:flex gap-4 flex-1">
@@ -25,7 +25,7 @@ const Navbar = () => {
       </div>
       {/* RIGHT LINKS */}
       <div className="hidden md:flex gap-4 items-center justify-end flex-1">
-        <div className="md:absolute top-3 r-2 flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
           <Image src="/phone.png" alt="" width={20} height={20} />
           <span>234 9063 199 422</span>
         </div>
