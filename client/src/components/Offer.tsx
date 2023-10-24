db05f16b02e1881fa41dbd87112c8d69bfcecca3
Emmyojile <emmaojile99@gmail.com>
2023-10-24 13:51:16 -0800

    featured

--- client/src/components/Offer.tsx
+++ client/src/components/Offer.tsx
@@ -1,8 +1,25 @@
+import Image from 'next/image'
 import React from 'react'
+import MyCountdownComponent from './MyCountdownComponent'
 
 const Offer = () => {
   return (
-    <div>Offer</div>
+    <div className='bg-black h-screen flex flex-col md:flex-row'>
+      {/* TEXT CONTAINER */}
+      <div className="flex-1 flex flex-col justify-center items-center text-center gap-8 p-6">
+        <h1 className="text-white text-5xl font-bold xl:text-6xl">
+        Delicious Burger & French Fry
+        </h1>
+        <p className='text-white xl:text-xl'>Progressively simplify effective e-toilers and process-centric methods
+          of empowerment. Quickly pontificate parallel.</p>
+          <MyCountdownComponent/>
+        <button className="bg-red-500 text-white rounded-md py-3 px-6">Order Now</button>
+      </div>
+      {/* IMAGE CONTAINER */}
+      <div className="flex-1 w-full relative">
+        <Image src={"/offerProduct.png"} alt='' fill className='object-contain'/>
+      </div>
+    </div>
   )
 }
 
