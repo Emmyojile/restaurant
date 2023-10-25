3b8fa8f350fd821aa293c66b483da32f400ffc52
Emmyojile <emmaojile99@gmail.com>
2023-10-25 15:44:56 -0800

    sungleproduct

--- client/src/components/Price.tsx
+++ client/src/components/Price.tsx
@@ -1,39 +1,52 @@
-import { type } from 'os'
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 type priceProps = {
-    price: number;
-    id: number;
-    options?: {title: string; additionalPrice: number}[]; 
-}
+  price: number;
+  id: number;
+  options?: { title: string; additionalPrice: number }[];
+};
 
-const Price = ({price,id,options}: priceProps) => {
+const Price = ({ price, id, options }: priceProps) => {
+  const [total, setTotal] = useState(price);
+  const [quantity, setQuantity] = useState(1);
+  const [selected, setSelected] = useState(0);
   return (
-    <div className='flex flex-col gap-4'>
-        <h2 className='text-2xl font-bold'>${price.toFixed(2)}</h2>
-        {/* OPTIONS CONTAINER */}
-        <div className="flex gap-4">
-            {options?.map((option) => (
-                <button className='p-2 ring-1 ring-blue-400 rounded-md' key={option.title}>{option.title}</button>
-            ))}
+    <div className="flex flex-col gap-4">
+      <h2 className="text-2xl font-bold">${total.toFixed(2)}</h2>
+      {/* OPTIONS CONTAINER */}
+      <div className="flex gap-4">
+        {options?.map((option, index) => (
+          <button
+            className=" min-w-[6rem] p-2 ring-1 ring-blue-400 rounded-md"
+            key={option.title}
+            style={{
+              background: selected === index ? "blue" : "white",
+              color: selected === index ? "white" : "blue",
+            }}
+            onClick={() => setSelected(index)}
+          >
+            {option.title}
+          </button>
+        ))}
+      </div>
+      {/* QUANTITY AND ADD BUTTON CONTAINER */}
+      <div className="flex justify-between items-center">
+        {/* QUANTITY */}
+        <div className="flex justify-between w-full p-3 ring-1 ring-blue-500">
+          <span>Quantity</span>
+          <div className="flex gap-4 items-center">
+            <button onClick={() =>setQuantity((prev) => (prev > 1 ? prev - 1 : 1))}>{"<"}</button>
+            <span>1</span>
+            <button onClick={() =>setQuantity((prev) => (prev < 9 ? prev + 1 : 9))}>{">"}</button>
+          </div>
         </div>
-        {/* QUANTITY AND ADD BUTTON CONTAINER */}
-        <div className="flex justify-between items-center">
-            {/* QUANTITY */}
-            <div className='flex justify-between w-full p-3 ring-1 ring-blue-500'>
-                <span>Quantity</span>
-                <div>
-                    <button>{'<'}</button>
-                    <span>1</span>
-                    <button>{'>'}</button>
-                </div>
-            </div>
-            {/* CART BUTTON */}
-            <button className='uppercase w-56 bg-blue-500 text-white ring-1 p-3'>
-                Add tO Cart
-            </button>
-        </div>
+        {/* CART BUTTON */}
+        <button className="uppercase w-56 bg-blue-500 text-white ring-1 p-3">
+          Add tO Cart
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Price
+export default Price;
