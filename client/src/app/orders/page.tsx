05dc99ed5d8431ea67efdfc15abf132eb7032c22
Emmyojile <emmaojile99@gmail.com>
2023-10-26 00:51:38 -0800

    done

--- client/src/app/orders/page.tsx
+++ client/src/app/orders/page.tsx
@@ -1,7 +1,44 @@
 import React from "react";
 
 const OrdersPage = () => {
-  return <div>OrdersPage</div>;
+  return (
+    <div className=" p-4 lg:px-20 2xl:px-40">
+      <table className="w-full border-separate border-spacing-3">
+        <thead>
+          <tr className=" text-left">
+            <th className=" hidden md:block">Order ID</th>
+            <th>Date</th>
+            <th>Price</th>
+            <th className=" hidden md:block">Products</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="text-sm md:text-base odd:bg-red-50">
+            <td className=" hidden md:block py-6 px-1">123456789</td>
+            <td>26.10.2023</td>
+            <td>$26.10.</td>
+            <td>Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1l</td>
+            <td>On the Way (approx. 10min)...</td>
+          </tr>
+          <tr className="text-sm md:text-base odd:bg-gray-100">
+            <td className=" hidden md:block py-6 px-1">123456789</td>
+            <td>26.10.2023</td>
+            <td>$26.10.</td>
+            <td>Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1l</td>
+            <td>On the Way (approx. 10min)...</td>
+          </tr>
+          <tr className="text-sm md:text-base odd:bg-gray-100">
+            <td className=" hidden md:block py-6 px-1">123456789</td>
+            <td>26.10.2023</td>
+            <td>$26.10.</td>
+            <td>Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1l</td>
+            <td>On the Way (approx. 10min)...</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default OrdersPage;
--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-blue-500  flex items-center justify-between'>
+    <div className='h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-blue-500  flex items-center justify-between'>
       <Link href="/" className='font-bold text-xl'>WESTERN-EATERY</Link>
       <p>ALL RIGHTS RESERVED</p>
     </div>
